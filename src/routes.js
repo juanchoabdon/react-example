55fcb2cba33e387f12f8d5717850fdef21fba64e
Juan Diego Sanchez <juan.sanchez@rappi.com>
2018-04-19 02:58:43 -0500

    a

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,6 @@ import store from 'store';
 import Home from 'containers/home/Home';
 import Login from 'containers/login/Login';
 import Signup from 'containers/signup/Signup';
-import Invitation from 'containers/invitation/Invitation';
 import NotFound from 'containers/not-found/NotFound';
 import NotAuth from 'containers/not-auth/NotAuth'
 import Admin from 'containers/admin/Admin';
@@ -21,7 +20,6 @@ const MainRoutes = () =>  {
                     <AppRoute path="/" exact component={Home} />
                     <AppRoute path="/login" exact component={Login} />
                     <AppRoute path="/signup" exact component={Signup} />
-                    <AppRoute path="/invitation/:token" component={Invitation} />
                     <AppRoute auth="true" path="/admin" component={Admin} />
                     <Route path="/404" component={NotFound} />      
                     <Route path="/403" component={NotAuth} />   
